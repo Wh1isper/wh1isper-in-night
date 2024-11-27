b1c46585aeb78e9caa7928ba9509c00af333c857
Wh1isper <jizhongsheng957@gmail.com>
2024-11-27 14:36:47 +0800

    Update hello example

--- config/config.go
+++ config/config.go
@@ -7,12 +7,25 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetToken() string {
+type Config struct {
+	Token string
+	App   string
+	Guild string
+}
+
+func GetConfig() Config {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	token := os.Getenv("DISCORD_BOT_TOKEN")
-	return token
+	app := os.Getenv("DISCORD_APPLICATION_ID")
+	guild := os.Getenv("DISCORD_GUILD_ID")
+
+	return Config{
+		Token: token,
+		App:   app,
+		Guild: guild,
+	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,115 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
 
-	"github.com/wh1isper/wh1isper-in-night/config"
+	"github.com/bwmarrin/discordgo"
+	"github.com/wh1isper/wh1isper-in-night/config" // Replace with your actual project import path
 )
 
+var BotToken = config.GetConfig().Token
+var appID = config.GetConfig().App
+var guildID = config.GetConfig().Guild
+
+type optionMap = map[string]*discordgo.ApplicationCommandInteractionDataOption
+
+func parseOptions(options []*discordgo.ApplicationCommandInteractionDataOption) (om optionMap) {
+	om = make(optionMap)
+	for _, opt := range options {
+		om[opt.Name] = opt
+	}
+	return
+}
+
+func interactionAuthor(i *discordgo.Interaction) *discordgo.User {
+	if i.Member != nil {
+		return i.Member.User
+	}
+	return i.User
+}
+
+func handleEcho(s *discordgo.Session, i *discordgo.InteractionCreate, opts optionMap) {
+	builder := new(strings.Builder)
+	if v, ok := opts["author"]; ok && v.BoolValue() {
+		author := interactionAuthor(i.Interaction)
+		builder.WriteString("**" + author.String() + "** says: ")
+	}
+	builder.WriteString(opts["message"].StringValue())
+
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: builder.String(),
+		},
+	})
+
+	if err != nil {
+		log.Panicf("could not respond to interaction: %s", err)
+	}
+}
+
+var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:        "echo",
+		Description: "Say something through a bot",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "message",
+				Description: "Contents of the message",
+				Type:        discordgo.ApplicationCommandOptionString,
+				Required:    true,
+			},
+			{
+				Name:        "author",
+				Description: "Whether to prepend message's author",
+				Type:        discordgo.ApplicationCommandOptionBoolean,
+			},
+		},
+	},
+}
+
 func main() {
-	fmt.Println(config.GetToken())
+	// Open a new Discord session
+
+	// Register the echo command handler
+	session, _ := discordgo.New("Bot " + BotToken)
+
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.Type != discordgo.InteractionApplicationCommand {
+			return
+		}
+
+		data := i.ApplicationCommandData()
+		if data.Name != "echo" {
+			return
+		}
+
+		handleEcho(s, i, parseOptions(data.Options))
+	})
+
+	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as %s", r.User.String())
+	})
+
+	_, err := session.ApplicationCommandBulkOverwrite(appID, guildID, commands)
+	if err != nil {
+		log.Fatalf("could not register commands: %s", err)
+	}
+
+	err = session.Open()
+	if err != nil {
+		log.Fatalf("could not open session: %s", err)
+	}
+
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt)
+	<-sigch
+
+	err = session.Close()
+	if err != nil {
+		log.Printf("could not close session gracefully: %s", err)
+	}
 }
